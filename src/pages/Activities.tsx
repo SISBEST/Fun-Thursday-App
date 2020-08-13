414f0962340194c7216c26fbf471a5148231932f
Samuel Sharp <samuelsharp4@gmail.com>
2020-08-13 05:59:04 -0700

    Modify activities & schedule.

--- src/pages/Activities.tsx
+++ src/pages/Activities.tsx
@@ -17,13 +17,17 @@ const Tab1: React.FC = () => {
             <li>Ticket To Ride</li>
             <li>Trivia</li>
             <li>Dinner Games</li>
+            <li>Mille Bournes</li>
+            <li>Battleship</li>
+            <li>Weird Things Humans Search For</li>
+            <li>Monopoly</li>
+            <li>Exploding Kittens</li>
           </ul>
           <li>Performances &amp; Shows</li>
           <ul>
             <li>Concert (3:00 PM - Living Room)</li>
             <li>Minecraft ban appeals, but you lose brain cells. (8:30 AM, 10:00 AM, 3:30 PM - HT1)</li>
           </ul>
-          <li></li>
         </ul>
       </IonContent>
     </IonPage>
--- src/pages/Schedule.tsx
+++ src/pages/Schedule.tsx
@@ -12,18 +12,16 @@ const Tab1: React.FC = () => {
       </IonHeader>
       <IonContent>
         <ul>
-          <li>8:00 AM - Breakfast</li>
-          <li>8:30 AM - All Activities Open</li>
-          <li>9:00 AM - Pillowfighting</li>
+          <li>8:30 AM - Breakfast</li>
+          <li>9:00 AM - Family Game</li>
+          <li>9:30 AM - Pillowfighting</li>
           <li>9:15 AM - Family Game</li>
-          <li>10:00 AM - All Activities Open</li>
+          <li>10:00 AM - Scavenger Hunt</li>
           <li>11:00 AM - Lunch</li>
-          <li>11:30 AM - Scavenger Hunt</li>
-          <li>Noon - All Activities Open</li>
           <li>1:00 PM - Independent Time</li>
           <li>2:00 PM - Snack</li>
           <li>2:15 PM - Bike Ride</li>
-          <li>3:00 PM - All Activities Open</li>
+          <li>3:00 PM - Family Game</li>
           <li>4:00 PM - Hill Rolling</li>
           <li>5:00 PM - Family Game</li>
           <li>6:00 PM - Dinner</li>
